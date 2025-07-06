aabff73a537ad5d0db3824196b6f4e1b10d0b223
coderrrrr <saadwaqar211@gmail.com>
2025-07-06 14:33:45 +0500

    Added Value class

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 # (No content at all)
-# Implement autodiff engine here
+# Implement auto diff engine here
+
+class val:
+    def __init__(self, data):
+        self.data = data
+    
+    def __repr__(self):
+        return f"Value(data={self.data})"
+        
+    def __add__(self, other):
+        return val(self.data + other.data)
+    
+    def __mul__(self, other):
+        return val(self.data * other.data)
+    
+a = val(2)
+b = val(-5)
+print(a*b)
